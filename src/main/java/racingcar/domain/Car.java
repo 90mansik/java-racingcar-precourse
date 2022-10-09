df4e251dd7f130bfdfba3cf6fbe8bc4ab7a80a05
mansik <90mansik@gmail.com>
2022-10-09 22:44:37 +0900

    feat: 자동차 "전진" 기능 구현한다.
    - 전진 기능 구현
    - 이동 유효성 체크 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,14 +2,33 @@ package racingcar.domain;
 
 public class Car {
     private final String name;
-    private final String position;
+    private int position;
 
-    public Car(String name, String position) {
+    public Car(String name, int position) {
         this.name = name;
         this.position = position;
     }
 
-    public static boolean validCarName(String carName) {
+    public boolean validCarName(String carName) {
         return carName.length() < 6;
+    }
+
+    public boolean validMove(int randomNo) {
+        return randomNo >=4;
+    }
+
+    public  int move(int randomNo) {
+        if(validMove(randomNo)){
+            this.position++;
+        }
+        return this.position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
