d9170ab3c4f2308e44f12eddb1dce44036372070
mansik <90mansik@gmail.com>
2022-10-10 00:30:20 +0900

    test: 레이싱시스템 랜덤 수 유효성 검증 구현

--- src/test/java/racingcar/domain/RacingSystemTest.java
+++ src/test/java/racingcar/domain/RacingSystemTest.java
@@ -1,17 +1,24 @@
 package racingcar.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class RacingSystemTest {
 
+    private RacingSystem racingSystem;
+
+    @BeforeEach
+    public void setUp() {
+        racingSystem = new RacingSystem();
+    }
+
     @Test
-    public void 쉼표로_이름_분리(){
-        RacingSystem racingSystem = new RacingSystem();
+    public void 쉼표로_이름_분리() {
         //given
         String name = "pobi,crong,honux";
         //when
@@ -22,4 +29,26 @@ class RacingSystemTest {
         Assertions.assertThat(cars.get(2).getName()).isEqualTo("honux");
 
     }
+
+    @Test
+    public void 랜덤_숫자_생성_검증() {
+        //when
+        int randomNo = racingSystem.getRandomNo();
+        //then
+        Assertions.assertThat(randomNo).isNotNull();
+    }
+
+    @Test
+    @DisplayName("1~9사이의 숫자가 생성되는지 유효성 검증")
+    public void 랜덤_숫자_유효성_검증() {
+        //given
+        int randomNo = racingSystem.getRandomNo();
+        //when
+        boolean result = randomNo >= 0 && randomNo <= 9 ? true : false;
+        //then
+        Assertions.assertThat(result).isTrue();
+
+    }
+
+
 }
