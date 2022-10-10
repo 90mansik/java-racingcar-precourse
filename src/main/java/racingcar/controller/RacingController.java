ca86ace10f18c6b21cebd9461ac0a1bc5be14d01
mansik <90mansik@gmail.com>
2022-10-10 20:23:04 +0900

    feat: 자동차이름/시도횟수 에러 발생시 계속 진행 구현
    - 이름/시도횟수 입력 시 에러가 발생하더라도 그 자리에서 계속 실행 될 수 있도록 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -10,24 +10,45 @@ public class RacingController {
     OutputView outputView = new OutputView();
 
     public void play() {
+        RacingSystem racingSystem = new RacingSystem();
 
-        try {
-            RacingSystem racingSystem = new RacingSystem();
+        setUpCars(racingSystem);
+        setUpFinishPosition(racingSystem);
 
-            racingSystem.setUpCars(inputView.getCarNames());
-            racingSystem.setUpFinishPosition(inputView.getFinishPosition());
-
-            for (int i = 0; i < racingSystem.getFinishPosition(); i++) {
-                movingCar(racingSystem);
-                outputView.resultMovingString(racingSystem);
-            }
-            outputView.finalVictoryCarString(racingSystem);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        for (int i = 0; i < racingSystem.getFinishPosition(); i++) {
+            movingCar(racingSystem);
+            outputView.resultMovingString(racingSystem);
         }
+        outputView.finalVictoryCarString(racingSystem);
 
 
     }
+
+    public void setUpCars(RacingSystem racingSystem) {
+        boolean status = true;
+        do {
+            try {
+                racingSystem.setUpCars(inputView.getCarNames());
+                status = false;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (status);
+
+    }
+
+    public void setUpFinishPosition(RacingSystem racingSystem) {
+        boolean status = true;
+        do {
+            try {
+                racingSystem.setUpFinishPosition(inputView.getFinishPosition());
+                status = false;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (status);
+    }
+
     public void movingCar(RacingSystem racingSystem) {
         for (int i = 0; i < racingSystem.getCars().size(); i++) {
             racingSystem.getCars().get(i).move(racingSystem.getRandomNo());
